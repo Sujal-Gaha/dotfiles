4d82abcc9c8b0a6b1f0e7b66a54641f3c067b15a
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-12-15 16:10:18 +0545

    handling dotfiles with stow

---
+++ scripts/backup.sh
@@ -0,0 +1,22 @@
+echo "Setting up dotfiles..."
+
+# List of packages to stow
+PACKAGES="vim"
+
+# Dry run first
+for pkg in $PACKAGES; do
+    if [ -d "$pkg" ]; then
+        echo "Stowing $pkg..."
+        stow --simulate "$pkg"
+    fi
+done
+
+read -p "Continue? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    for pkg in $PACKAGES; do
+        if [ -d "$pkg" ]; then
+            stow --adopt "$pkg"
+        fi
+    done
+fi
