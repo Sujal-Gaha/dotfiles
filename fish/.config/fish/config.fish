29762f84ce5eeba49c07495467f6f9e008328ca1
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-12-31 11:49:24 +0545

    updated config for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,42 +22,5 @@ if status is-interactive # Commands to run in interactive sessions can go here
     alias ls 'eza --icons'
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias q 'qs -c ii'
-
-    # Git aliases
-    alias g 'git'
-    alias ga 'git add'
-    alias gaa 'git add --all'
-    alias gb 'git branch'
-    alias gs 'git status'
-    alias gc 'git commit'
-
-    alias mkdir 'mkdir -p'
-
-    # Helpful aliases
-    alias l 'eza -lh --icons=auto'
-    alias la 'ls -lAh'
-    alias ld 'eza -lhD --icons=auto'
-    alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
-    alias ls 'ls --color=tty'
-    alias lsa 'ls -lah'
-    alias lt 'eza --icons=auto --tree'
-    alias c 'clear'                                                        # clear terminal
-    alias un '$aurhelper -Rns'                                             # uninstall package
-    alias up '$aurhelper -Syu'                                             # update system/package/aur
-    alias pl '$aurhelper -Qs'                                              # list installed package
-    alias pa '$aurhelper -Ss'                                              # list available package
-    alias pc '$aurhelper -Sc'                                              # remove unused cache
-    alias po '$aurhelper -Qtdq | $aurhelper -Rns -'                        # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
-    alias fastfetch 'fastfetch --logo-type kitty'
-
-    # Directory navigation shortcuts
-    alias .. 'cd ..'
-    alias ... 'cd ../..'
-    alias .3 'cd ../../..'
-    alias .4 'cd ../../../..'
-    alias .5 'cd ../../../../..'
-
-
+    
 end
-fnm env --use-on-cd | source
-set -gx WATCHPACK_POLLING true
